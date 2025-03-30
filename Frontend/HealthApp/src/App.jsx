bc4bd31ea6f8e6aba5fb869b8ddc8381f27702a2
Mugagga Kimera <mugagga.kimera04@gmail.com>
2025-03-30 20:30:56 +0100

    Added basic routing

--- Frontend/HealthApp/src/App.jsx
+++ Frontend/HealthApp/src/App.jsx
@@ -1,12 +1,22 @@
 
 import './App.css'
-
+import Home from './Home/Home'
+import Login from './Login/Login'
+import Register from './Register/Register'
+import Dashboard from './Dashboard/Dashboard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-      <h1>Health App home</h1>
-    </>
+<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path='*' element={<Home />} />
+    </Routes>
+</BrowserRouter>
   )
 }
 
