6bb3bb79e460cb7b397d2653dc2b1354d778156e
Mugagga Kimera <mugagga.kimera04@gmail.com>
2025-04-03 02:13:00 +0100

    changed port value to enviroment variable

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,7 @@ const Database = require('./src/config/Database');
 const models = require('./src/models');
 
 const authenticationRoutes = require('./src/routes/authenticationRoutes');
-const port = 2556;
-require("./src/Services/Register")
-require("./src/Services/Login")
+const port = process.env.PORT;
 
 app.use(BodyParser.json());
 app.use(session({
@@ -25,7 +23,9 @@ app.use(session({
         maxAge:(14*24*60*60*1000),
     }
 }))
+
 app.use("/api/v1",authenticationRoutes);
+
 app.listen(port, (err) => {
     if (err){
         console.log("An error has occurred");
