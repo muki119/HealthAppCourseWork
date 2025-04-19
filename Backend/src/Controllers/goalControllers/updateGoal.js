94737b4c6f2f780b51d29b7f27d31977efa857e1
Mugagga Kimera <mugagga.kimera04@gmail.com>
2025-04-19 19:54:46 +0100

    fixed undefined goalId

--- Backend/src/Controllers/goalControllers/updateGoal.js
+++ Backend/src/Controllers/goalControllers/updateGoal.js
@@ -3,7 +3,7 @@ const {updateGoalService} = goalServices
 const updateGoalController = async (req, res,next) => {
     try {
         const userId = req.session.user.id
-        const goalId = req.params.goalid
+        const {goalId} = req.params
         const goalData = req.body
         await updateGoalService(userId,goalId,goalData);
         return res.status(200).end();
